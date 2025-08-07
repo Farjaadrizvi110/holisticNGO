48ffe686319f5dd419311f0819a4316f47706fd1
Farjaadrizvi110 <farjaadrizvi@gmail.com>
2025-08-07 22:11:51 +0500

    fixed website

--- HFC/settings.py
+++ HFC/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--3nx$3qs)2p6qekh-z0god1p&uw-2r$q*_nqi6!1m@o+3+pgwz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]  # Allow all hosts for development; restrict in production
 
